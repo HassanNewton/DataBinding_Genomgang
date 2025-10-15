8135d66c6d7adca4bf6271bd0515ac04819a9459
HassanNewton <hassan.hussin@newton.se>
2025-10-15 10:50:27 +0200

    Add project files.

---
+++ DataBinding_Genomgang/MainWindow_Binding.xaml.cs
@@ -0,0 +1,87 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows;
+
+namespace DataBinding_Genomgang
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window, INotifyPropertyChanged
+    {
+        private string _name;
+        public string FirstName
+        {
+            get
+            {
+                return _name;
+            }
+            set
+            {
+                _name = value;
+                //OnPropertyChanged("Name");
+                //OnPropertyChanged(nameof(Name));
+                OnPropertyChanged();
+                OnPropertyChanged(nameof(FullName));
+            }
+        }
+
+        private string _lastName;
+        public string LastName
+        {
+            get
+            {
+                return _lastName;
+            }
+            set
+            {
+                _lastName = value;
+                //OnPropertyChanged("Name");
+                //OnPropertyChanged(nameof(Name));
+                OnPropertyChanged();
+                OnPropertyChanged(nameof(FullName));
+            }
+        }
+
+        public string FullName
+        {
+            get
+            {
+                return $"{FirstName} {LastName}";
+            }
+        }
+
+        public List<String> Countries { get; set; } = new List<string> { "Sverige", "Norge", "Finland" };
+
+        private string _selectedCountry;
+        public string SelectedCountry
+        {
+            get { return _selectedCountry; }
+            set
+            {
+                _selectedCountry = value;
+                OnPropertyChanged();
+            }
+        }
+
+
+
+
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = this;
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        private void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+
+
+}
